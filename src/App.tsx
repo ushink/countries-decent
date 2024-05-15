4a189bf8d8b7912a528fd89463f5f5d95d7e069c
Ulyana <ushinkevich@mail.ru>
2024-05-15 07:58:17 +0400

    add routing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
+import { Countries } from "./pages/Countries/Countries";
+import { NotFound } from "./pages/NotFound/NotFound";
+import { CountryDetail } from "./pages/CountryDetail/CountryDetail";
 
 function App() {
-  return <div className="App">Hello word!</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Countries />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/country/:id" element={<CountryDetail />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
