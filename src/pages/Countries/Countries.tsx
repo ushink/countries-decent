61735affc8e726b8175dcd725bfa3f94ce58d085
Ulyana <ushinkevich@mail.ru>
2024-05-15 07:56:38 +0400

    folder pages

---
+++ src/pages/Countries/Countries.tsx
@@ -0,0 +1,9 @@
+import s from "./Countries.module.css";
+
+export function Countries() {
+  return (
+    <div className={s.countries}>
+      <h1>Countries</h1>
+    </div>
+  );
+}
---
+++ src/pages/CountryDetail/CountryDetail.tsx
@@ -0,0 +1,9 @@
+import s from "./CountryDetail.module.css";
+
+export function CountryDetail() {
+  return (
+    <div className={s.container}>
+      <h1>Country Detail</h1>
+    </div>
+  );
+}
